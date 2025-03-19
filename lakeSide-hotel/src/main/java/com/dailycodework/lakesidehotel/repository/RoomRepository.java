2f153f4dcae25e2b21ce6a2e043df7f14349bafc
Feshchenko1 <114470355+Feshchenko1@users.noreply.github.com>
2025-03-19 19:22:06 +0200

    Back-end entities

--- lakeSide-hotel/src/main/java/com/dailycodework/lakesidehotel/repository/RoomRepository.java
+++ lakeSide-hotel/src/main/java/com/dailycodework/lakesidehotel/repository/RoomRepository.java
@@ -1,4 +1,8 @@
 package com.dailycodework.lakesidehotel.repository;
 
-public interface RoomRepository {
+import com.dailycodework.lakesidehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
